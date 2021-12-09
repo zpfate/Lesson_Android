73326a3633e3283991077b9c90c7e38b2b1edd39
TwistedFate <783919313@qq.com>
2021-12-09 18:51:42 +0800

    Button 点击

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,17 +2,54 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    static final String TAG = "fate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         /// 设置视图
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.edit_text);
 
 
+        Button btn = findViewById(R.id.btn);
+        // 点击事件
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.e(TAG,"onClick");
+            }
+        });
+
+        btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Log.e(TAG,"onLongClick");
+                return false;
+            }
+        });
+
+        btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.e(TAG,"onTouch");
+                return false;
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        Log.e(TAG,"onClick");
     }
 }
