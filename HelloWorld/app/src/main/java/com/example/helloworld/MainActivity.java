fc8b75c9bbe6b84877e5970c0cf53e361a40fec1
zpfate <783919313@qq.com>
2022-01-19 10:01:52 +0800

    重置工程

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,20 +19,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /// 设置视图
-        setContentView(R.layout.edit_text);
+        /// textView
+//        setContentView(R.layout.activity_main);
+//        TextView tv_one = findViewById(R.id.tv_one);
+//        tv_one.setText("leo");
 
+        createEditText();
+    }
+
+
+    public void createEditText() {
+        setContentView(R.layout.edit_text);
+        EditText editText = findViewById(R.id.edittext);
+        Button btn = findViewById(R.id.getBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editText.getText().toString();
+                Log.e(TAG, "onClick: 输入的内容： " + text );
+            }
+        });
+    }
+
+
+    public void createBtn() {
+        /// 按钮
+        setContentView(R.layout.button);
 
         Button btn = findViewById(R.id.btn);
-        // 点击事件
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                Log.e(TAG,"onClick");
-//            }
-//        });
+//        // 点击事件
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Log.e(TAG,"onClick");
+            }
+        });
+        /// 长按事件
         btn.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
@@ -39,15 +64,17 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
+        /// 触摸事件
         btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                Log.e(TAG,"onTouch");
+                Log.e(TAG,"onTouch" + motionEvent.getAction());
+                /// return true会消费事件
                 return false;
             }
         });
     }
+
 
     public void onClick(View view) {
         Log.e(TAG,"onClick");
